38caae1e99c361316d4175bccff3764ca4481fda
rennvers <revaniafitria@gmail.com>
2026-04-22 19:05:20 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -73,8 +73,8 @@ def create_time_segment_df(df):
 
 # Load cleaned data
 
-day_df = pd.read_csv("day.csv")
-hour_df = pd.read_csv("hour.csv")
+day_df = pd.read_csv("dashboard/day.csv")
+hour_df = pd.read_csv("dashboard/hour.csv")
 
 day_df["dteday"] = pd.to_datetime(day_df["dteday"])
 hour_df["dteday"] = pd.to_datetime(hour_df["dteday"])
